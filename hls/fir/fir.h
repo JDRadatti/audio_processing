2f5639a30f6669422e8b1a61dcef909047c7486c
JDRadatti <jdradatti@gmail.com>
2025-12-16 07:39:37 -0800

    Update for AXI packet protocol

--- hls/fir/fir.h
+++ hls/fir/fir.h
@@ -8,15 +8,20 @@
 #include "hls_stream.h"
 #include "ap_int.h"
 
-const int N=128;
-
 typedef ap_int<24> data_t;
 typedef ap_int<16> coef_t;
 typedef ap_int<48> acc_t;
 
+struct transPkt {
+    data_t data;
+    ap_uint<1> last;
+    ap_uint<8> keep;
+    ap_uint<8> strb;
+};
+
 void fir (
-    hls::stream<data_t> &in_stream,
-    hls::stream<data_t> &out_stream
+    hls::stream<transPkt> &in_stream,
+    hls::stream<transPkt> &out_stream
   );
 
 #endif
--- hls/fir/fir_test.cpp
+++ hls/fir/fir_test.cpp
@@ -14,32 +14,45 @@ int main () {
   const int    SAMPLES=600;
   FILE         *fp, *fin;
 
-  hls::stream<data_t> in_stream;
-  hls::stream<data_t> out_stream;
-
-  data_t signal, output;
-  int i;
+  hls::stream<transPkt> in_stream;
+  hls::stream<transPkt> out_stream;
 
   fin = fopen("input.dat", "r");
   fp  = fopen("out.dat", "w");
+  if (!fin || !fp) {
+      printf("ERROR: Cannot open input/output file. You may need to run gen_fir_data.py. \n");
+      return 1;
+  }
 
   // Feed all input samples into the stream
-  for (i = 0; i < SAMPLES; i++) {
+  for (int i = 0; i < SAMPLES; i++) {
       int tmp;
       fscanf(fin, "%d", &tmp);
-      signal = tmp;
-      in_stream.write(signal);
+      if (fscanf(fin, "%d", &tmp) != 1) {
+          printf("ERROR: unexpected EOF in input.dat\n");
+          fclose(fin);
+          fclose(fp);
+          return 1;
+      }
+
+      transPkt pkt;
+      pkt.data = tmp;
+      pkt.keep = 0xFF;
+      pkt.strb = 0xFF;
+      pkt.last = (i == SAMPLES - 1) ? 1 : 0;
+      in_stream.write(pkt);
   }
 
   // Run the FIR Filter
   fir(in_stream, out_stream);
 
   // Retrieve results from output stream
-  for (i = 0; i < SAMPLES; i++) {
+  for (int i = 0; i < SAMPLES; i++) {
       if (!out_stream.empty()) {
-          output = out_stream.read();
-          fprintf(fp, "%d\n", (int)output);
-          printf("%3d  in=%d  out=%d\n", i, (int)signal, (int)output);
+          transPkt output = out_stream.read();
+          fprintf(fp, "%d\n", (int)output.data);
+          printf("%3d out=%d\n", i, (int)output.data);
+          if (output.last) { break; }
       } else {
           printf("Warning: output stream empty at sample %d\n", i);
           break;
